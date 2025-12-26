a9c956673e8a2b1c409cda7fda344b80159541c6
Yahel <yahaly2000@gmail.com>
2025-12-26 12:05:54 +0200

    react snippets and lualine

--- init.lua
+++ init.lua
@@ -725,6 +725,14 @@ require('lazy').setup({
           return 'make install_jsregexp'
         end)(),
         dependencies = {
+          {
+            'mlaursen/vim-react-snippets',
+            opts = {
+              readonly_props = true, -- Set to `false` if all props should no longer be wrapped in `Readonly<T>`.
+              test_framework = '@jest/globals', -- Set to "vitest" if you use vitest
+              test_renderer_path = '@testing-library/user-event', -- Set to a custom test renderer. For example "@/test-utils"
+            },
+          },
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,34 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      component_separators = '',
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+      lualine_b = { { 'filename', path = 1 }, 'branch' },
+      lualine_c = {
+        '%=', --[[ add your center components here in place of this comment ]]
+      },
+      lualine_x = {},
+      lualine_y = { 'filetype', 'progress' },
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_pading = 2 },
+      },
+    },
+    inactive_sections = {
+      lualine_a = {
+        { 'filename', path = 1 }, -- 👈 relative to cw
+      },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
+  },
+}
